18283c5a87b0c713b0412265b74dc6be4deda90b
Alexandre Bergeron <alexandre.bergeron@axiontech.ca>
2018-10-06 17:58:28 -0400

    Avance en ligne droite et correction de d'autre bugs avec accélération.

--- src/main.ino
+++ src/main.ino
@@ -23,7 +23,8 @@ Date: 01 oct 2018
 /*******************************************************************************
  * Prototypes locaux
 *******************************************************************************/
-  void SerialPrintf(const char *fmt, ...);
+void SerialPrintf(const char *fmt, ...);
+char* strFloat(float valeur);
 
 /*******************************************************************************
  * Variables locales
@@ -31,6 +32,7 @@ Date: 01 oct 2018
 
 float g_leftSpeed = 0.0;
 float g_rightSpeed = 0.0;
+char floatbuffer[8]; //mémoire reservé pour afficher des floats
 
 /*******************************************************************************
  * fonctions
@@ -52,20 +54,36 @@ void SerialPrintf(const char *fmt, ...)
   Serial.print(buffer);
 }
 
+/**
+ * @brief quand on fait %f, ça affiche des ? à la place. Work around avec %s et cette fonction.
+ *  La fonction devrait afficher tout les nombres voulu, à deux décimal.
+ */
+char* strFloat(float valeur)
+{
+  //20 car 2 pour décimal, 1 pour la virgule, 1 pour le signe et 1 pour le caractères NULL. Ça laisse 3 byte pour le reste.
+  memset(floatbuffer, 0, sizeof(floatbuffer));
+  if(valeur < 100.99 && valeur > -100.99)
+  {
+    memset(floatbuffer, 0, sizeof(floatbuffer));
+    dtostrf((double)valeur, sizeof(floatbuffer)-2, 2, floatbuffer);
+  }
+  return floatbuffer;
+}
+
 float fSpeedAdjustment(){
   float fAdjustement = 0.0;
   int32_t i32DeltaPulse = ENCODER_Read(LEFT) - ENCODER_Read(RIGHT);
   
   if(i32DeltaPulse > MOVE_WHEEL_MAX_ENCODER_DELTA){
-     i32DeltaPulse = MOVE_WHEEL_MAX_ENCODER_DELTA;
+    i32DeltaPulse = MOVE_WHEEL_MAX_ENCODER_DELTA;
   }
 
   if(i32DeltaPulse < -1*MOVE_WHEEL_MAX_ENCODER_DELTA){
     i32DeltaPulse = -1*(MOVE_WHEEL_MAX_ENCODER_DELTA);
   }
 
-  fAdjustement = i32DeltaPulse * MOVE_DERIVATIVE_ADJUSTEMENT_FACTOR;
-  Serial.println(fAdjustement);
+  fAdjustement = (float)i32DeltaPulse * MOVE_DERIVATIVE_ADJUSTEMENT_FACTOR;
+  SerialPrintf("vitesse ajuste: %s\n", strFloat(fAdjustement));
   return fAdjustement;
 }
 
@@ -77,7 +95,7 @@ float fSpeedAdjustment(){
  */
 int32_t MOVE_getDistanceMM(int ID)
 {
-  int32_t d = ENCODER_Read(ID)*(1/MOVE_PULSE_PER_TURN)*MOVE_WHEEL_DIAMETER*PI;
+  int32_t d = ENCODER_Read(ID)*((float)1/MOVE_PULSE_PER_TURN)*MOVE_WHEEL_DIAMETER*PI;
   return d;
 }
 
@@ -98,11 +116,20 @@ void MOVE_vAccelerationSingleWheel(float finalSpeed, unsigned int time, int ID)
   float deltaSpeed = finalSpeed - *currentSpeed;
   float speedIncrementation = deltaSpeed * (float) wait / (float) time;
 
-  while(*currentSpeed < finalSpeed)
+  unsigned int iCount;
+  for(iCount =0; iCount <= ((time/wait)+((time%wait)>0)); iCount++)
   {
+    char buffer1[8] = {0};
+    char buffer2[8] = {0}; 
+    strcpy(buffer1, strFloat(*currentSpeed));
+    strcpy(buffer2, strFloat(finalSpeed));    
+    SerialPrintf("Acceleration %s sur %s\n", buffer1, buffer2);
     *currentSpeed += speedIncrementation;
     MOTOR_SetSpeed(ID, *currentSpeed);
   }
+  //Set la vitesse à cause que les floats ne sont pas très précis (on ne sera jamais à la consigne exacte sinon)
+  *currentSpeed = finalSpeed;
+  MOTOR_SetSpeed(ID, *currentSpeed);
 }
 
 /**
@@ -119,14 +146,26 @@ void MOVE_vAcceleration(float finalSpeed, unsigned int time)
 
   //S'assure que la roue droite soit à la même vitesse que la roue gauche.
   g_rightSpeed = g_leftSpeed;
-  while(g_leftSpeed != finalSpeed)
+  unsigned int iCount;
+  for(iCount =0; iCount <= ((time/wait)+((time%wait)>0)); iCount++)
   {
+    char buffer1[20] = {0};
+    char buffer2[20] = {0}; 
+    strcpy(buffer1, strFloat(g_leftSpeed));
+    strcpy(buffer2, strFloat(finalSpeed));    
+    SerialPrintf("Acceleration %s sur %s\n", buffer1, buffer2);
     g_leftSpeed += speedIncrementation;
     g_rightSpeed += speedIncrementation;
     MOTOR_SetSpeed(RIGHT, g_rightSpeed);
     MOTOR_SetSpeed(LEFT, g_leftSpeed);
     delay(wait);
   }
+
+  //Set la vitesse à cause que les floats ne sont pas très précis (on ne sera jamais à la consigne exacte sinon)
+  g_leftSpeed = finalSpeed;
+  g_rightSpeed = finalSpeed;
+  MOTOR_SetSpeed(LEFT, g_leftSpeed);
+  MOTOR_SetSpeed(RIGHT, g_leftSpeed);
 }
 
 void MOVE_vAvancer(float fVitesse, int32_t i32Distance_mm){
@@ -136,7 +175,9 @@ void MOVE_vAvancer(float fVitesse, int32_t i32Distance_mm){
   MOVE_vAcceleration(fVitesse, 200);
   while(MOVE_getDistanceMM(LEFT) < i32Distance_mm)
   {
+    SerialPrintf("Distance fait = %i mm\n", MOVE_getDistanceMM(LEFT));
     g_rightSpeed = fVitesse + fSpeedAdjustment();
+    MOTOR_SetSpeed(RIGHT, g_rightSpeed);
     delay(100);
   }
 }
@@ -156,16 +197,21 @@ void MOVE_Rotation1Roue(unsigned int angle, int iRotationDirection)
   int32_t angleEnDistance = (2*PI*MOVE_LARGEUR_ROBOT*angle)/360;
   //Arrête les mouvements avant de tourner
   MOVE_vAcceleration(0.0, 200);
+  //Reset les encodeurss
+  ENCODER_Reset(0);
+  ENCODER_Reset(1);
+
   MOVE_vAccelerationSingleWheel(0.9, 200 ,ID);
   int32_t distanceActuelle = MOVE_getDistanceMM(ID);
 
   while( distanceActuelle < angleEnDistance)
   {
-    SerialPrintf("Virage à %s de %i degré, distance = %i sur %i", 
+    SerialPrintf("Virage à %s de %i degré, distance = %i sur %i\n", 
       iRotationDirection == LEFT ? "LEFT": "RIGTH", angle, distanceActuelle, angleEnDistance);
     delay(50);
     distanceActuelle = MOVE_getDistanceMM(iRotationDirection == LEFT ? RIGHT: LEFT);
   }
+  MOVE_vAccelerationSingleWheel(0, 300, ID);
 }
 
 
@@ -182,8 +228,14 @@ void setup(){
 
 void loop() {
   // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
+  SerialPrintf("test %i sur %i\n", 2, 5);
+  SerialPrintf("Avance pour 2m\n");
+  MOVE_vAvancer(0.7,4000);
+  SerialPrintf("Pivote de 180 degre\n");
+  MOVE_Rotation1Roue(180,RIGHT);
+  SerialPrintf("Avance pour 2m\n");
   MOVE_vAvancer(0.7,2000);
-  MOVE_Rotation1Roue(90,RIGHT);
-
+  MOVE_vAcceleration(0, 300);
   delay(2000);
+  while(1)delay(200);
 }
